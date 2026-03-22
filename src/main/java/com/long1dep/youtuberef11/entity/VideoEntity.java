419b5547905b22074299206807735d6fb4d42e86
= <hlong03022k6@gmail.com>
2026-03-22 15:01:50 +0700

    setup entity

---
+++ src/main/java/com/long1dep/youtuberef11/entity/VideoEntity.java
@@ -0,0 +1,19 @@
+package com.long1dep.youtuberef11.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "video")
+@Getter
+@Setter
+public class VideoEntity extends AbstractAuditingEntity<String>{
+    @Id
+    @UuidGenerator
+    private String id;
+
+    @Column(name = "url",  nullable = false)
+    private String url;
+}
---
+++ src/test/java/com/long1dep/youtuberef11/Youtuberef11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.long1dep.youtuberef11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Youtuberef11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
